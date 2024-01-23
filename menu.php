ac23993f033a16ea012e2d2d8e46be9e4480e1c5
manju prasad <manjuprasad.4327@gmail.com>
2024-01-23 08:38:47 +0530

    Manju

---
+++ menu.php
@@ -0,0 +1,67 @@
+<style>
+    /* Add this to your CSS stylesheet */
+.navbar-nav .nav-item.active {
+    background-color: Red; 
+}
+
+.navbar-nav .nav-item.active a {
+    color: #fff; 
+}
+</style>
+<header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.php">
+            <span>ExcelSquare</span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php"> About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="products.php">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="price.php">Download</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="price.php">FAQs</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="price.php">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="price.php">Users say</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+            </ul>
+            <!-- <div class="quote_btn-container">
+              <form class="form-inline">
+                <button class="btn   nav_search-btn" type="submit">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+              </form>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>
+                  Call : +01 123455678990
+                </span>
+              </a>
+            </div> -->
+          </div>
+        </nav>
+      </div>
+    </header>
