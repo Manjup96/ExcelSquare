83580c33fd25f2a11a06a51fbc09cc2946f6e8a6
manju prasad <manjuprasad.4327@gmail.com>
2024-01-24 11:42:43 +0530

    Manju

--- footer.php
+++ footer.php
@@ -1,113 +1,127 @@
 <!-- info section -->
 
 <section class="info_section layout_padding2">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3">
-          <div class="info_contact">
-            <h4>
-              Address
-            </h4>
-            <div class="contact_link_box">
-              <a href="">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                <span>
-                  Location
-                </span>
-              </a>
-              <a href="">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-                <span>
-                  Call +01 1234567890
-                </span>
-              </a>
-              <a href="">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <span>
-                  demo@gmail.com
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="info_social">
-            <a href="">
-              <i class="fa fa-facebook" aria-hidden="true"></i>
-            </a>
-            <a href="">
-              <i class="fa fa-twitter" aria-hidden="true"></i>
-            </a>
-            <a href="">
-              <i class="fa fa-linkedin" aria-hidden="true"></i>
-            </a>
-            <a href="">
-              <i class="fa fa-instagram" aria-hidden="true"></i>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_link_box">
-            <h4>
-              Links
-            </h4>
-            <div class="info_links">
-              <a class="active" href="index.html">
-                <img src="images/nav-bullet.png" alt="">
-                Home
-              </a>
-              <a class="" href="about.html">
-                <img src="images/nav-bullet.png" alt="">
-                About
-              </a>
-              <a class="" href="service.html">
-                <img src="images/nav-bullet.png" alt="">
-                Services
-              </a>
-              <a class="" href="price.html">
-                <img src="images/nav-bullet.png" alt="">
-                Pricing
-              </a>
-              <a class="" href="contact.html">
-                <img src="images/nav-bullet.png" alt="">
-                Contact Us
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="info_detail">
-            <h4>
-              Info
-            </h4>
-            <p>
-              necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful
-            </p>
-          </div>
-        </div>
-        <div class="col-md-3 mb-0">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="info_contact">
           <h4>
-            Subscribe
+            Address
           </h4>
-          <form action="#">
-            <input type="text" placeholder="Enter email" />
-            <button type="submit">
-              Subscribe
-            </button>
-          </form>
+          <div class="contact_link_box">
+            <a href="">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <span>
+                Location
+              </span>
+            </a>
+            <a href="">
+              <i class="fa fa-phone" aria-hidden="true"></i>
+              <span>
+                Call +01 1234567890
+              </span>
+            </a>
+            <a href="">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+              <span>
+                demo@gmail.com
+              </span>
+            </a>
+          </div>
+        </div>
+        <div class="info_social">
+          <a href="">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+          </a>
+          <a href="">
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+          </a>
+          <a href="">
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+          </a>
+          <a href="">
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+          </a>
         </div>
       </div>
+      <div class="col-md-3">
+        <div class="info_link_box">
+          <h4>
+            Links
+          </h4>
+          <div class="info_links">
+            <a class="active" href="index.php">
+              <img src="images/nav-bullet.png" alt="">
+              Home
+            </a>
+            <a class="" href="about.php">
+              <img src="images/nav-bullet.png" alt="">
+              About
+            </a>
+            <a class="" href="products.php">
+              <img src="images/nav-bullet.png" alt="">
+              Products
+            </a>
+            <a class="" href="faq.php">
+              <img src="images/nav-bullet.png" alt="">
+              Faq
+            </a>
+            <a class="" href="contact.php">
+              <img src="images/nav-bullet.png" alt="">
+              Contact Us
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="info_detail">
+          <h4>
+            Info
+          </h4>
+          <div class="info_links">
+
+            <a class="" href="terms.php">
+              <img src="images/nav-bullet.png" alt="">
+              Terms
+            </a>
+            <a class="" href="privacy.php">
+              <img src="images/nav-bullet.png" alt="">
+              Privacy
+            </a>
+            <a class="" href="support.php">
+              <img src="images/nav-bullet.png" alt="">
+              Support
+            </a>
+            <a class="" href="licenses.php">
+              <img src="images/nav-bullet.png" alt="">
+              Licenses
+            </a>
+            <a class="" href="security.php">
+              <img src="images/nav-bullet.png" alt="">
+              Security
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 mt-4">
+        <a class="navbar-brand" href="index.php">
+          <img src="images/logo.png" alt="ExcelSquare Image">
+        </a>
+      </div>
     </div>
-  </section>
+  </div>
+</section>
 
-  <!-- end info section -->
+<!-- end info section -->
 
 
-  <!-- footer section -->
-  <footer class="footer_section">
-    <div class="container">
-      <p>
-        &copy; <span id="displayYear"></span> All Rights Reserved By
-        <a href="https://html.design/">Free Html Templates</a>
-      </p>
-    </div>
-  </footer>
-  <!-- footer section -->
+<!-- footer section -->
+<footer class="footer_section">
+  <div class="container">
+    <p>
+      &copy; <span id="displayYear"></span> All Rights Reserved By
+      <a href="https://html.design/">Free Html Templates</a>
+    </p>
+  </div>
+</footer>
+<!-- footer section -->
