19e91627ff5869f4c4b9735165528f701797a622
savitha153 <138104938+savitha153@users.noreply.github.com>
2024-01-24 13:44:04 +0530

    savitha

--- about.php
+++ about.php
@@ -28,8 +28,36 @@
   <link href="css/style.css" rel="stylesheet" />
   <!-- responsive style -->
   <link href="css/responsive.css" rel="stylesheet" />
-
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
+<style>
+    .custom-container {
+            /* max-width: 800px; Adjust the maximum width as needed */
+            margin-left: 100px; /* Adjust the left margin to move it slightly to the left */
+            /* margin-right: 15px; Add right margin to keep the content centered */
+        }
+
+      
+         /* Media query for tablets (e.g., iPad) */
+         @media (max-width: 768px) {
+            .custom-container {
+                margin-left: 10px; /* Adjust the left margin for tablet view */
+                margin-right: 10px; /* Adjust the right margin for tablet view */
+            }
+        }
+
+        /* Media query for mobile devices */
+        @media (max-width: 576px) {
+            .custom-container {
+                margin-left: 5px; /* Adjust the left margin for mobile view */
+                margin-right: 5px; /* Adjust the right margin for mobile view */
+            }
+        }
+        #img-box img {
+            width: 80%;
+            height: auto;
+        }
+</style>
 
 <body class="sub_page">
 
@@ -41,38 +69,47 @@
     <!-- end header section -->
   </div>
 
-  <!-- about section -->
-
-  <section class="about_section layout_padding">
-    <div class="container  ">
+ 
+  <section class="mt-5">
+    <div class="container-fluid custom-container">
       <div class="row">
         <div class="col-md-6">
           <div class="detail-box">
             <div class="heading_container">
-              <h2>
+              <!-- <h2>
                 About Us
-              </h2>
+              </h2> -->
             </div>
-            <p>
-              Words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks </p>
-            <a href="">
+            <h2 style="font-family:epilogue-sansserif">“more simplicity, more productivity” </h2>
+            <p style="text-align: justify; font-size:18px; font-family:epilogue-sansserif">We started this venture, Excel Square with a goal of providing simple tools and solutions that would make everyday life easier for professionals across the world. The focus is on enhancing productivity through simplicity. Our experience of the fact that, complex problems often just need a stroke of simplicity for the solution, keeps us on the lookout of areas which typically have nagging problems or areas in which we can create a big value add through the introduction of simple and effective utilities. The first step towards this was the creation of X-TrackAmaze, an extremely effective planning tool for a professional whose plate is full of “things to be done” at all times. Its easy interface, its utility, its effectiveness, attractive pricing, all these won the hearts of thousands of users worldwide. In fact, across its versions, the number of downloads of X-TrackAmaze had exceeded 200,000. Building on top of this success, we have launched PlanDeer, which is the next avatar of X-TrackAmaze. While PlanDeer carries the heart of X-TrackAmaze, it additionally includes several features and functionalities to increase the agility and productivity of the tool. We have strived hard to make the usability of PlanDeer a notch above that of X-TrackAmaze, which itself had high focus on Usability.</p>
+            <p style="text-align: justify; font-size:18px; font-family:epilogue-sansserif">We firmly believe that, it is only when we enable YOU, as our customer to excel, we can excel, and hence provide meaning to our identity that is called Excel Square! We strongly feel that our belief, “more simplicity, more productivity” would help enable us advance towards our goal.</p>
+            <!-- <a href="">
               Read More
-            </a>
+            </a> -->
           </div>
         </div>
         <div class="col-md-6 ">
-          <div class="img-box">
-            <img src="images/about-img.png" alt="">
+          <div id="img-box">
+            <img src="images/about-us.jpg" alt="">
           </div>
         </div>
 
       </div>
     </div>
   </section>
+  <section class="mt-5">
+    <div class="aboutgrey">
+  <div class="container">
+    <h2 style="font-family:epilogue-sansserif">Team Behind Simplicity</h2>
+    <p style="text-align: justify; font-size:18px; font-family:epilogue-sansserif">This venture is promoted by a team whose varied experience in the field of software development and other domains provides a strong impetus to identify the areas where we can add tremendous value and help realize our goal – which is, to enable our users to excel better.</p>
 
+    <p style="text-align: justify; font-size:18px; font-family:epilogue-sansserif">The team led by Satish BN Rao, by nature, has a strong customer orientation and sense of values. It strongly believes in the fact that while the values-adds can be simple in nature, the impact they generate can be much larger and stronger. This has been the fundamental belief towards launching the Products of Excel Square.</p>
+
+    <p style="text-align: justify; font-size:18px; font-family:epilogue-sansserif">Satish has been in the IT industry for more than 20 years and has worked across spectrum of IT companies. He has been responsible for many of the huge successes that have been achieved by the organizations that he has worked for. With a strong affinity for product development, he has been instrumental in driving some of the key product initiatives of his organizations. He has also held high positions coupled with large responsibilities while managing large programs for some of the key clients. Known as a team-man, he has been a mentor to many of his erstwhile colleagues, enabling them to bloom as strong, efficient and successful managers, architects and team-personnel. His experience and desire to start a venture on his own, has resulted in the founding of Excel Square Tech Private Limited, a company that is focused on value added tools, products and specific services for the clients.</p>
+</div>
   <!-- end about section -->
-
-  
+</div>
+  </section>
 
 
   <!-- footer section -->
@@ -89,7 +126,9 @@
   <!-- custom js -->
   <script src="js/custom.js"></script>
 
-
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
